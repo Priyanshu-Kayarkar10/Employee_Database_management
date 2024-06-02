5103f896323b50941e79abd979c63fa94b84972c
Priyanshu <priyanshu.kayarkar009@gmail.com>
2024-06-02 16:05:06 +0530

    Edit Functionality Added

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 I just build Employee Database Management System using Vanilla Javascript,html,css
 
-`You can Add Employee Details and delete the employee details. I'll be adding more features to it. `
+# Features
+
+`- Add Employee Details`
+`- Delete the employee details`
+`- Edit the employee details`
--- script.js
+++ script.js
@@ -2,6 +2,97 @@
   const data = await fetch("data.json");
   const res = await data.json();
 
+  // Edit Employee
+  const editBtn = document.querySelector(".edit");
+  const editEmployee = document.querySelector(".editEmployee");
+  const editEmployeeForm = document.querySelector(".editEmployee_form");
+
+  // Printing the previous values LOGIC:
+  editBtn.addEventListener("click", (e) => {
+    editEmployee.style.display = "flex";
+
+    const {
+      firstName,
+      lastName,
+      dob,
+      Salary,
+      age,
+      address,
+      contactNumber,
+      email,
+      imageUrl,
+    } = selectedEmployee;
+
+   
+
+    [...editEmployeeForm].map((data, index) => {
+      
+      if (data.name === "firstName") {
+        data.value = firstName;
+      } else if (data.name === "lastName") {
+        data.value = lastName;
+      } else if (data.name === "imageUrl") {
+        data.value = imageUrl;
+      } else if (data.name === "Salary") {
+        data.value = String(Salary);
+      } else if (data.name === "email") {
+        data.value = email;
+      } else if (data.name === "address") {
+        data.value = address;
+      } else if (data.name === "dob") {
+        data.value = dob;
+      } else if (data.name === "contactNumber") {
+        data.value = String(contactNumber);
+      }
+    });
+  });
+
+  editEmployeeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formData = new FormData(editEmployeeForm);
+    const values = [...formData.entries()];
+    let empData = {};
+    values.forEach((val) => {
+      empData[val[0]] = val[1];
+    });
+
+    empData.imageUrl = empData.imageUrl || "https://svgur.com/i/16hm.svg";
+
+    const dobInput = document.querySelector(".editEmployee--dob");
+    dobInput.max = `${new Date().getFullYear() - 18} -${new Date()
+      .toISOString()
+      .slice(5, 10)}`;
+
+    empData.age =
+      new Date().getFullYear() - parseInt(empData.dob.slice(0, 4), 10);
+    employees.forEach((emp) => {
+      if (emp.id === Number(selectedEmployeeId)) {
+        if (employees[selectedEmployeeId]) {
+          let employee = employees[selectedEmployeeId];
+          employee.address = empData.address;
+          employee.email = empData.email;
+          employee.firstName = empData.firstName;
+          employee.lastName = empData.lastName;
+          employee.Salary = empData.Salary;
+          employee.contactNumber = empData.contactNumber;
+          employee.dob = empData.dob;
+          employee.age = empData.age;
+          employee.imageUrl = empData.imageUrl;
+        }
+      }
+    });
+
+    renderImployees();
+    editEmployeeForm.reset();
+    editEmployee.style.display = "none";
+  });
+
+  editEmployee.addEventListener("click", (e) => {
+    if (e.target.className === "editEmployee") {
+      editEmployee.style.display = "none";
+    }
+  });
+
   // button segment
   const createEmployee = document.querySelector(".createEmployee");
   const addEmployeeModal = document.querySelector(".addEmployee");
@@ -24,11 +115,14 @@
     let empData = {};
     values.forEach((val) => {
       empData[val[0]] = val[1];
-      console.log(empData);
     });
-    console.log(values);
 
-    empData.id = employees[employees.length - 1].id + 1;
+    // empData.id
+    if (employees[employees.length - 1] == undefined) {
+      empData.id = Number(employees.length);
+    } else {
+      empData.id = Number(employees[employees.length - 1].id + 1);
+    }
 
     empData.imageUrl = empData.imageUrl || "https://svgur.com/i/16hm.svg";
 
@@ -39,6 +133,8 @@
 
     empData.age =
       new Date().getFullYear() - parseInt(empData.dob.slice(0, 4), 10);
+
+
     employees.push(empData);
     renderImployees();
     addEmployeeForm.reset();
@@ -55,14 +151,14 @@
   //   const employeeItem = document.querySelector(".employes__name--item")
   const employeeInfo = document.querySelector(".employee__single--info");
 
-  // Select Employee LOGIC:
+  // Select Employee LOGIC and deleting employee
+
   employeeList.addEventListener("click", (e) => {
     if (e.target.tagName === "SPAN" && selectedEmployeeId != e.target.id) {
       selectedEmployeeId = e.target.id;
       renderImployees();
       // render Single Employee
     }
-
     if (e.target.tagName === "I") {
       employees = employees.filter(
         (emp) => String(emp.id) !== e.target.parentNode.id
